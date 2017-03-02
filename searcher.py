4a7fe23049154c1850d64ccc17814b34eb946655
Levi Hill <levi.hill.home@gmail.com>
2017-03-02 19:29:01 +0000

    Added methods for finding leaves, internal nodes and cheapest path from root to leaf

--- searcher.py
+++ searcher.py
@@ -19,3 +19,37 @@ class searcher:
 			if n.OutDegree == 0:
 				leaves.append(n)
 		return leaves
+
+	def findInternal(self,nodes):
+		internal = []
+		for n in nodes:
+			if n.OutDegree != 0:
+				internal.append(n)
+
+	def findRoot(self,nodes):
+		for n in nodes:
+			if n.label == "root":
+				return n
+
+	def findRootPath(leaf,edges):
+		rootPath = []
+		pathCost = 0
+		currentNode = leaf
+		rootPath.append(leaf)
+		while currentNode != "root":
+			for e in edges:
+				if e.end == currentNode:
+					rootPath.append(e.start)
+					currentNode = e.start
+					pathCost += e.weight
+		return pathCost,rootPath 
+
+	def findCheapestPath(self, graph):
+		cheapPath = []
+		cheapCost = 10000000000
+		leaves = self.findLeaves(graph.vertices)
+		for leaf in leaves:
+			cost,path = self.findRootPath(leaf,graph.edges)
+			if cost < cheapCost:
+				cheapPath = path
+		return cheapPath,cheapCost
